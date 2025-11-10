9a35afc984d365f9fda22e7d8b675956bef7598a
ranshull <anshulrawat047@gmail.com>
2025-11-10 14:00:52 +0530

    allowed ports

--- StudyBud-master/studybud/settings.py
+++ StudyBud-master/studybud/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chatroom-wnio.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
