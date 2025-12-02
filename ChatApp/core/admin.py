d4b6397e72afeafb910e85982c5af71d3ed19530
ranshull <anshulrawat047@gmail.com>
2025-12-02 16:20:58 +0530

    cleaning

--- ChatApp/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Message, PDFFile, Doubt, UserProfile
-
-admin.site.register(Message)
-admin.site.register(PDFFile)
-admin.site.register(Doubt)
-admin.site.register(UserProfile)
