8f88b937d4ba23de24ca4b407e435806454a8fd8
ranshull <anshulrawat047@gmail.com>
2025-12-07 15:09:03 +0530

    seet

--- StudyBud-master/studybud/settings.py
+++ StudyBud-master/studybud/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['chatroom-wnio.onrender.com', 'localhost', '127.0.0.1']
